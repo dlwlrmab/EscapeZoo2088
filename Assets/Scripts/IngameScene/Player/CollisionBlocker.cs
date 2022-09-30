20dc52a8944f04bc5eb9c0afb425758ef2fc1a74
PHC <lucidsun92@gmail.com>
2022-09-30 13:37:21 +0900

    [feat] 캐릭터 이동 수정
    캐릭터 마찰력 제거 (벽붙기 불가)
    다른 캐릭터 못 밀게 수정
    PlayerMe 프리팹에 수정사항 적용
    MoveTestScene 참고
    충돌Matrix 수정(Edit/Project Settings/Physics2d)
    캐릭터 Layer 추가 및 수정(충돌 시 구분)
    PlayerController 이름 변경 (PlayerMove)
    안쓰는 것 삭제(NonPhysics관련)

---
+++ Assets/Scripts/IngameScene/Player/CollisionBlocker.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionBlocker : MonoBehaviour
+{
+    public BoxCollider2D playerCollider;
+    public BoxCollider2D blockCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics2D.IgnoreCollision(playerCollider, blockCollider, true);
+    }
+}
---
+++ Assets/Scripts/IngameScene/Player/PlayerMove.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    float jumpHeight;
+
+    Rigidbody2D rb;
+    float inputDirection;
+
+    bool jump = false;
+    public bool isGrounded = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   // 입력만 처리
+        inputDirection = Input.GetAxis("Horizontal");
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(isGrounded)
+                jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {   // 물리 작용 처리
+        float jumpSpeed = rb.velocity.y;
+        if (jump)
+        {
+            isGrounded = false;
+            jumpSpeed += Mathf.Sqrt(2f * -Physics2D.gravity.y * jumpHeight);
+            jump = false;
+        }
+        rb.velocity = new Vector2(inputDirection * speed, jumpSpeed);
+    }
+
+    #region ##### Ground Check Trigger #####
+    private void OnTriggerEnter2D(Collider2D other)
+    {   // 땅이나 플레이어 위에 있는 경우는 점프 가능
+        int layer = other.gameObject.layer;
+        if (layer == LayerMask.NameToLayer("Ground") || layer == LayerMask.NameToLayer("Player"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        int layer = other.gameObject.layer;
+        if (layer == LayerMask.NameToLayer("Ground") || layer == LayerMask.NameToLayer("Player"))
+        {
+            isGrounded = false;
+        }
+    }
+    #endregion
+}
