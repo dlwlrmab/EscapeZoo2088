6cf96409aca27b110a31e113a47dbdf3ce57064a
dlwlrmab <97571449+dlwlrmab@users.noreply.github.com>
2022-10-01 14:58:50 +0900

    [fix] ground 설정이 되어있지않아 점프가 되지않는 현상 수정
    - ground 오브젝트 프리팹으로 설정
    - 용도별 BLOCK? 프리팹으로 생성
    - BLOCK 코드 추가

---
+++ Assets/Scripts/IngameScene/Round/BlockController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockController : MonoBehaviour
+{
+   enum BLOCKTYPE
+    {
+        NONE,
+        MOVEX,
+        MOVEY,
+    }
+
+    [SerializeField] BLOCKTYPE _type;
+    [SerializeField] float _speed = 0;
+
+    [SerializeField] float _startPosX;
+    [SerializeField] float _destPosX;
+    [SerializeField] float _startPosY;
+    [SerializeField] float _destPosY;
+
+    private bool _isReverse = true;
+
+    private void Awake()
+    {
+   
+    }
+
+
+    private void FixedUpdate()
+    {
+        if(_type == BLOCKTYPE.MOVEX)
+        {
+            if (!_isReverse)
+            {
+                transform.localPosition = new Vector3(transform.localPosition.x + 0.1f * _speed, transform.localPosition.y, 0);
+                if (transform.localPosition.x >= _destPosX)
+                    _isReverse = true;
+            }
+            else
+            {
+                transform.localPosition = new Vector3(transform.localPosition.x + -0.1f * _speed, transform.localPosition.y, 0);
+                if (transform.localPosition.x <= _startPosX)
+                    _isReverse = false;
+            }
+        }
+        else if(_type == BLOCKTYPE.MOVEY)
+        {
+            if (!_isReverse)
+            {
+                transform.localPosition = new Vector3(transform.localPosition.x , transform.localPosition.y + 0.1f * _speed, 0);
+                if (transform.localPosition.y >= _destPosY)
+                    _isReverse = true;
+            }
+            else
+            {
+                transform.localPosition = new Vector3(transform.localPosition.x , transform.localPosition.y + -0.1f * _speed, 0);
+                if (transform.localPosition.y <= _startPosY)
+                    _isReverse = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/IngameScene/Round/Round5.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Round5 : Round
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+}
