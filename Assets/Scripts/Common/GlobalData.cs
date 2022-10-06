ae956538c7a2454149336b1c1dd418c3352894e5
dlwlrmab <97571449+dlwlrmab@users.noreply.github.com>
2022-10-06 19:23:27 +0900

    [chore] loginscene ui 변경

--- Assets/Scripts/Common/GlobalData.cs
+++ Assets/Scripts/Common/GlobalData.cs
@@ -10,4 +10,6 @@ public class GlobalData : MonoBehaviour
     public static string id = string.Empty;
     public static bool isHost = true;  // 한 그룹내에서 여러명이 게임을 시작할수도 있어, 호스트 지정이 필요하거나, 인원수가 차면 자동으로 시작하거나 하는 규칙이 필요할듯?
     public static List<Player> playerList = new List<Player>(); // 함께 플레이하는 유저들의 데이터 저장
+
+    public static string GatewayAPI = "https://opupgoihqd.execute-api.ap-northeast-2.amazonaws.com/test/";
 }
--- Assets/Scripts/LobbyScene/LoginScene.cs
+++ Assets/Scripts/LobbyScene/LoginScene.cs
@@ -12,10 +12,19 @@ using UnityEngine.UI;
 
 public class LoginScene : MonoBehaviour
 {
+    [SerializeField] Transform _startCanvas;
+    [SerializeField] Transform _Canvas;
+    [SerializeField] Transform _loginCanvas;
+    [SerializeField] Transform _joinCanvas;
+
     // 로그인기능 완성시까지 로그인, 가입 버튼 비활성화
     // 바로 게임시작버튼으로 인게임화면 로비화면 진입
     [SerializeField] Transform _loginButton;
+    [SerializeField] Transform _joinButton;
+
     [SerializeField] Text _inputId;
+    [SerializeField] Text _inputPW;
+    [SerializeField] Text _inputMBTI;
     [SerializeField] Text _notiText;
 
     SceneLoadManager _scenemanager = null;
@@ -24,15 +33,59 @@ public class LoginScene : MonoBehaviour
     {
         _scenemanager = SceneLoadManager.Instance;
         _scenemanager.PlayFadeIn();
+        _Canvas.gameObject.SetActive(false);
+        _startCanvas.gameObject.SetActive(true);
+    }
+
+    public void OnClickGoLogin()
+    {
+        _notiText.text = "로그인";
+
+        _startCanvas.gameObject.SetActive(false);
+        _Canvas.gameObject.SetActive(true);
+        _loginCanvas.gameObject.SetActive(true);
+        _loginButton.gameObject.SetActive(true);
+        _joinButton.gameObject.SetActive(false);
+        _inputId.transform.parent.gameObject.SetActive(true);
+        _inputPW.transform.parent.gameObject.SetActive(true);
+        _inputMBTI.transform.parent.gameObject.SetActive(false);
+    }
+
+    public void OnClickGoJoin()
+    {
+        _notiText.text = "회원가입";
+
+        _startCanvas.gameObject.SetActive(false);
+        _Canvas.gameObject.SetActive(true);
+        _joinCanvas.gameObject.SetActive(true);
+        _loginButton.gameObject.SetActive(false);
+        _joinButton.gameObject.SetActive(true);
+        _inputId.transform.parent.gameObject.SetActive(true);
+        _inputPW.transform.parent.gameObject.SetActive(true);
+        _inputMBTI.transform.parent.gameObject.SetActive(true);
+    }
+
+    public void OnClickExit()
+    {
+        _startCanvas.gameObject.SetActive(true);
+        _Canvas.gameObject.SetActive(false);
+        initText();
     }
 
     public void OnClickLoginButton()
     {
         string id = _inputId.text;
+        string pw = _inputPW.text;
 
         if (id == string.Empty || id == null)
         {
             _notiText.text = "ID를 입력하지 않았습니다.";
+            return;
+        }
+
+        if (pw == string.Empty || pw == null)
+        {
+            _notiText.text = "PW를 입력하지 않았습니다.";
             return;
         }
 
@@ -43,10 +96,24 @@ public class LoginScene : MonoBehaviour
     public void OnClickJoinButton()
     {
         string id = _inputId.text;
+        string pw = _inputPW.text;
+        string mbti = _inputMBTI.text;
 
         if (id == string.Empty || id == null)
         {
             _notiText.text = "ID를 입력하지 않았습니다.";
+            return;
+        }
+
+        if (pw == string.Empty || pw == null)
+        {
+            _notiText.text = "PW를 입력하지 않았습니다.";
+            return;
+        }
+
+        if (!CheckMBTI(mbti))
+        {
+            _notiText.text = "잘못된 MBTI 입니다.";
             return;
         }
 
@@ -87,7 +154,7 @@ public class LoginScene : MonoBehaviour
         var webClient = new WebClient();
         webClient.Headers[HttpRequestHeader.ContentType] = "application/json";
         var responseBytes
-            = webClient.UploadString(new Uri("https://opupgoihqd.execute-api.ap-northeast-2.amazonaws.com/test/") + "Login", "POST"
+            = webClient.UploadString(new Uri(GlobalData.GatewayAPI) + "Login", "POST"
             , JsonConvert.SerializeObject(req));
 
         //var webClient = new WebClient();
@@ -108,6 +175,7 @@ public class LoginScene : MonoBehaviour
         else
         {
             GlobalData.id = res.userId;
+            initText();
             RecvLoginResult(true);
         }
     }
@@ -124,11 +192,11 @@ public class LoginScene : MonoBehaviour
             mbti = "infj",
         };
 
-        int i = 0;
+        int i = 1;
         if (i == 0)
         {
             string json = JsonUtility.ToJson(req);
-            StartCoroutine(RequestPost("https://opupgoihqd.execute-api.ap-northeast-2.amazonaws.com/test/AccountJoin/", json));
+            StartCoroutine(RequestPost(GlobalData.GatewayAPI, json));
         }
         else
         {
@@ -137,7 +205,7 @@ public class LoginScene : MonoBehaviour
             webClient.Headers[HttpRequestHeader.ContentType] = "application/json";
 
             var responseBytes
-            = webClient.UploadString(new Uri("https://opupgoihqd.execute-api.ap-northeast-2.amazonaws.com/test/") + "AccountJoin", "POST"
+            = webClient.UploadString(new Uri(GlobalData.GatewayAPI) + "AccountJoin", "POST"
             , JsonConvert.SerializeObject(req));
 
             var res = JsonConvert.DeserializeObject<ResAccountJoin>(responseBytes);
@@ -155,9 +223,46 @@ public class LoginScene : MonoBehaviour
             {
                 _notiText.text = "이미 존재하는 ID입니다.";
             }
+            else if(res.ResponseType == ResponseType.Success)
+            {
+                _notiText.text = "회원가입 성공, 로그인 해주세요";
+                initText();
+            }
+            else
+            {
+                _notiText.text = "회원가입 실패";
+            }
         }
     }
 
+    void initText()
+    {
+        _inputId.text = null;
+        _inputPW.text = null;
+        _inputMBTI.text = null;
+    }
+
+    bool CheckMBTI(string _mbti)
+    {
+        string mbti = _mbti.ToLower();
+
+        if (mbti == null || string.IsNullOrEmpty(mbti) || mbti.Length != 4)
+            return false;
+
+        if (mbti[0] != 'i' && mbti[0] != 'e')
+            return false;
+
+        if (mbti[1] != 's' && mbti[1] != 'n')
+            return false;
+
+        if (mbti[2] != 't' && mbti[2] != 'f')
+            return false;
+
+        if (mbti[3] != 'j' && mbti[3] != 'p')
+            return false;
+
+        return true;
+    }
     // 서버연결 테스트용 함수
     // 연결 완료후 삭제
     IEnumerator RequestPost(string URL, string json)
