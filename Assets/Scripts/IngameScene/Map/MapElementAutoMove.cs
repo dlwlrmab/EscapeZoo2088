95ce29de77841d54105df729ba51966cc71632fa
hong hye ryeong <h0412l@naver.com>
2022-10-08 15:12:44 +0900

    [feat] 눈 맵 효과 추가

--- Assets/Scripts/IngameScene/Map/MapElementAutoMove.cs
+++ Assets/Scripts/IngameScene/Map/MapElementAutoMove.cs
@@ -4,27 +4,48 @@ using UnityEngine;
 
 public class MapElementAutoMove : MonoBehaviour
 {
-    [SerializeField] private bool _left = false;
+    public enum DIRECTION
+    {
+        LEFT,
+        RIGHT,
+        UP,
+        DOWN
+    }
+
+    [SerializeField] private DIRECTION _direction = DIRECTION.RIGHT;
     [SerializeField] private float _moveSpeed = 10;
     [SerializeField] private float _rotSpeed = 0;
 
-    int mapSize = 10;
+    int mapWidth = 10;
+    int mapHeight = 7;
 
     void Update()
     {
         transform.Rotate(Vector3.forward, Time.deltaTime * _rotSpeed);
 
-        if (_left)
+        if (_direction == DIRECTION.LEFT)
         {
             transform.localPosition += Vector3.left * Time.deltaTime * _moveSpeed;
-            if (transform.localPosition.x <= -mapSize)
-                transform.localPosition = new Vector3(mapSize, transform.localPosition.y);
+            if (transform.localPosition.x <= -mapWidth)
+                transform.localPosition = new Vector3(mapWidth, transform.localPosition.y);
         }
-        else
+        else if (_direction == DIRECTION.RIGHT)
         {
             transform.localPosition += Vector3.right * Time.deltaTime * _moveSpeed;
-            if (transform.localPosition.x >= mapSize)
-                transform.localPosition = new Vector3(-mapSize, transform.localPosition.y);
+            if (transform.localPosition.x >= mapWidth)
+                transform.localPosition = new Vector3(-mapWidth, transform.localPosition.y);
+        }
+        else if (_direction == DIRECTION.UP)
+        {
+            transform.localPosition += Vector3.up * Time.deltaTime * _moveSpeed;
+            if (transform.localPosition.y >= mapHeight)
+                transform.localPosition = new Vector3(transform.localPosition.x, -mapHeight);
+        }
+        else if (_direction == DIRECTION.DOWN)
+        {
+            transform.localPosition += Vector3.down * Time.deltaTime * _moveSpeed;
+            if (transform.localPosition.y <= -mapHeight)
+                transform.localPosition = new Vector3(transform.localPosition.x, mapHeight);
         }
     }
 }
