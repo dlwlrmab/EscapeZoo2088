4345d0252df2ab4a1db27a9e720bad8a80464487
PHC <lucidsun92@gmail.com>
2022-10-10 14:56:11 +0900

    [test] 디버그Text 추가

---
+++ Assets/Scripts/RealDebug/RuntimeDebugText.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RuntimeDebugText : MonoBehaviour
+{
+    private Text debugText;
+    private List<Player> players = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        debugText = GetComponent<Text>();
+        players = IngamePlayerController.Instance.GetPlayerList();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        debugText.text = "";
+        foreach(var player in players)
+        {
+            debugText.text += player.name + " " + player.gameObject.GetComponent<Rigidbody2D>().position + "\n";
+        }
+    }
+}
