53727f0e5f9d43f1da207c352b50b317f647bd78
Hyeryeong Hong <h0412l@naver.com>
2022-10-11 19:43:57 +0900

    [fix] 준비 완료될 때까지 스토리 로딩 대기

--- Assets/Scripts/IngameScene/IngameScene.cs
+++ Assets/Scripts/IngameScene/IngameScene.cs
@@ -91,6 +91,7 @@ public class IngameScene : MonoBehaviour
         _mapController.LoadRound(); // 라운드 셋팅 먼저
         _playerController.LoadRound();
         _uiController.LoadRound();
+        _loadingController.HideStartLoading();
         _loadingController.LoadRoundLoading();
     }
 
