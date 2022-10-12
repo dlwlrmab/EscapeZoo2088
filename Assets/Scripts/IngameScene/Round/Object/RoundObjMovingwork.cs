7b30743a17735c1a37b029d13e97af61ca424c05
Hyeryeong Hong <h0412l@naver.com>
2022-10-12 21:52:02 +0900

    [fix] 무빙워크 나만 되도록 수정

--- Assets/Scripts/IngameScene/Round/Object/RoundObjMovingwork.cs
+++ Assets/Scripts/IngameScene/Round/Object/RoundObjMovingwork.cs
@@ -5,46 +5,26 @@ using UnityEngine;
 public class RoundObjMovingwork : MonoBehaviour
 {
     [SerializeField] private bool _front = false;
-    [SerializeField] private Transform _autoMove;
     [SerializeField] private SpriteRenderer _spriteRenderer;
 
-    private Transform _playerPerent;
     private float _spriteInitSizeX = 0;
 
-    private List<Rigidbody2D> playerRbList = new List<Rigidbody2D>();
+    private Rigidbody2D _myPlayer;
 
     private void Awake()
     {
-        _playerPerent = IngameScene.Instance.PlayerController.transform;
         _spriteInitSizeX = _spriteRenderer.size.x;
         _spriteRenderer.transform.localRotation = new Quaternion(0, _front ? 180 : 0, 0, 0);
     }
 
-    void Update()
+    private void FixedUpdate()
     {
-        // 올라탄 플레이어가 없다면 움직임 멈추기
-        /*
-        if (_autoMove.transform.childCount == 0)
-        {
-            if (_autoMove.transform.localPosition.x != 0)
-                _autoMove.transform.localPosition = Vector3.zero;
-        }
-        else
-        {
-            _autoMove.transform.localPosition += (_front ? Vector3.right : Vector3.left) * Time.deltaTime * 1f;
-        }
-        */
         _spriteRenderer.size += Vector2.right * Time.deltaTime * 1.7f;
         if (_spriteRenderer.size.x >= _spriteInitSizeX + 0.5f)
             _spriteRenderer.size = new Vector2(_spriteInitSizeX, _spriteRenderer.size.y);
-    }
 
-    private void FixedUpdate()
-    {
-        foreach (var rb in playerRbList)
-        {
-            rb.velocity += Vector2.left * 1.2f;
-        }
+        if (_myPlayer != null)
+            _myPlayer.velocity += Vector2.left * 1.2f;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -52,8 +32,9 @@ public class RoundObjMovingwork : MonoBehaviour
         int layer = other.gameObject.layer;
         if (layer == LayerMask.NameToLayer("Player"))
         {
-            //other.transform.parent = _autoMove;
-            playerRbList.Add(other.GetComponent<Rigidbody2D>());
+            Player player = other.GetComponent<Player>();
+            if (player.IsMine)
+                _myPlayer = other.GetComponent<Rigidbody2D>();
         }
     }
 
@@ -62,8 +43,9 @@ public class RoundObjMovingwork : MonoBehaviour
         int layer = other.gameObject.layer;
         if (layer == LayerMask.NameToLayer("Player"))
         {
-            //other.transform.parent = _playerPerent;
-            playerRbList.Remove(other.GetComponent<Rigidbody2D>());
+            Player player = other.GetComponent<Player>();
+            if (player.IsMine)
+                _myPlayer = null;
         }
     }
 }
